62e938a6a404aa970cac4fda45882d3e221fbde2
KseniiaSm <143532668+KseniiaSm@users.noreply.github.com>
2023-12-04 20:59:32 +0300

    Добавлен механизм наследования. Добавлен скидочный продукт — специальный продукт, цена которого снижена наразмер скидки. У скидки есть также срок действия. После завершения срокадействия скидка меняется.

---
+++ src/main/java/com/HomeWork07/Product.java
@@ -0,0 +1,43 @@
+package com.HomeWork07;
+
+public class Product {
+    private String nameOfProduct;
+    private int price;
+
+    public Product() {
+
+    }
+
+    @Override
+    public String toString() {
+        return nameOfProduct;
+    }
+
+    public String getNameOfProduct() {
+        return nameOfProduct;
+    }
+
+    public void setNameOfProduct(String nameOfProduct) {
+        if (nameOfProduct.matches("\\d+")) {
+            System.out.println("Название продукта не может содержать только цифры");
+        } else {
+            this.nameOfProduct = nameOfProduct;
+        }
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price > 0) {
+            this.price = price;
+        } else {
+            System.out.println("Цена не может быть отрицательной или равной 0");
+        }
+    }
+}
+
+
+
