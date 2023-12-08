c6ee63626123515d21a4cb71664bf3e60ea74751
KseniiaSm <143532668+KseniiaSm@users.noreply.github.com>
2023-12-08 21:57:41 +0300

    HomeWork08

--- src/main/java/com/Homework08/App.java
+++ src/main/java/com/Homework08/App.java
@@ -1,58 +1,70 @@
 package com.Homework08;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
 
         boolean isEmpty = true;
 
-        Person[] allPeople;
-        allPeople = new Person[3];
-        for (int i = 0; i < allPeople.length; i++) {
-            System.out.println("Введите свое имя");
-            allPeople[i] = new Person();
-            allPeople[i].setName(scanner.next() + ' ' + scanner.next());
-            System.out.println("Введите колличество денег на счете (целое число).");
-            if (scanner.hasNextInt()) {
-                allPeople[i].setMoneySum(scanner.nextInt());
-            } else {
-                System.out.println("Неверный формат ввода");
-                break;
-            }
+        String inputFilePath = "C:\\Users\\Ксения\\IdeaProjects\\Innopolis\\src\\main\\java\\com\\Homework08\\data";
+        File file = new File(inputFilePath);
+        Scanner scanner = new Scanner(file);
+
+        String outputFilePath = "C:\\Users\\Ксения\\IdeaProjects\\Innopolis\\src\\main\\java\\com\\Homework08\\dataResult";
+        PrintWriter printWriter = new PrintWriter(outputFilePath);
+
+        String line = scanner.nextLine();
+        String moneySum = line.replaceAll("[^\\d\\s]", " ").replaceAll("\\s+", " ").strip();
+        String [] moneySumArrayString = moneySum.split(" ");
+        int [] moneySumOfAllPeople = new int[3];
+        int counter = 0;
+        for (String moneySumOfPerson : moneySumArrayString) {
+            moneySumOfAllPeople[counter++] = Integer.parseInt(moneySumOfPerson);
         }
 
-        Product[] allProducts;
-        allProducts = new Product[5];
-        for (int j = 0; j < allProducts.length; j++) {
-            System.out.println("Введите название продукта");
-            allProducts[j] = new Product();
-            allProducts[j].setNameOfProduct(scanner.next());
-            System.out.println("Введите цену данного продукта");
-            if (scanner.hasNextInt()) {
-                allProducts[j].setPrice(scanner.nextInt());
-            } else {
-                System.out.println("Неверный формат ввода");
-                break;
-            }
+        String names = line.replaceAll("[^a-zA-Zа-яА-Я=]"," ").replaceAll("\\s+", " ").strip();
+        String [] namesArray = names.split("\\s+=\\s?+");
+
+        Person[] allPeople = new Person[3];
+        for (int i = 0; i < allPeople.length; i++) {
+            allPeople[i] = new Person();
+            allPeople[i].setName(namesArray[i]);
+            allPeople[i].setMoneySum(moneySumOfAllPeople[i]);
+        }
+
+        String lineSecond = scanner.nextLine();
+        String priceAll = lineSecond.replaceAll("[^\\d\\s]", " ").replaceAll("\\s+", " ").strip();
+        String [] priceAllArrayString = priceAll.split(" ");
+        int [] priceArray = new int[5];
+        int counter2 = 0;
+        for (String priceEachString : priceAllArrayString) {
+            priceArray[counter2++] = Integer.parseInt(priceEachString);
+        }
+
+        String nameOfProduct = lineSecond.replaceAll("[^a-zA-Zа-яА-Я=]"," ").replaceAll("\\s+", " ").strip();
+        String [] nameOfProductArray = nameOfProduct.split("\\s+=\\s?+");
+
+        Product[] allProducts = new Product[5];
+        for (int i = 0; i < allProducts.length; i++) {
+            allProducts[i] = new Product();
+            allProducts[i].setNameOfProduct(nameOfProductArray[i]);
+            allProducts[i].setPrice(priceArray[i]);
         }
 
         for (int k = 0; k < allPeople.length; k++) {
             for (int m = 0; m < allProducts.length; m++) {
                 if (allProducts[m].getPrice() <= allPeople[k].getMoneySum()) {
-                    System.out.println(allPeople[k].getName() + " купил(а) " + allProducts[m].getNameOfProduct());
+                    printWriter.println(allPeople[k].getName() + " купил(а) " + allProducts[m].getNameOfProduct());
                     allPeople[k].setMoneySum(allPeople[k].getMoneySum() - allProducts[m].getPrice());
                     allPeople[k].packetWithProducts[m] = allProducts[m];
                 } else {
-                    System.out.println(allPeople[k].getName() + " не может позволить себе " + allProducts[m].getNameOfProduct());
+                    printWriter.println(allPeople[k].getName() + " не может позволить себе " + allProducts[m].getNameOfProduct());
                 }
             }
-            System.out.println(allPeople[k].getName() + " купил(а) " + Arrays.toString(allPeople[k].packetWithProducts));
+            printWriter.println(allPeople[k].getName() + " купил(а) " + Arrays.toString(allPeople[k].packetWithProducts));
             for (int i = 0; i < allPeople[k].packetWithProducts.length; i++) {
                 if (allPeople[k].packetWithProducts[i] != null) {
                     isEmpty = false;
@@ -62,10 +74,11 @@ public class App {
                 }
             }
             if (isEmpty) {
-                System.out.println(allPeople[k].getName() + " ничего не может позволить себе");
+                printWriter.println(allPeople[k].getName() + " ничего не куплено.");
             }
         }
-
+        scanner.close();
+        printWriter.close();
 
     }
 }
